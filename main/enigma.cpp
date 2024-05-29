cb7cb3ea636ea1ee7ec462969e0ee6fd483823ac
Boris <boris.fox.solovyev@gmail.com>
2024-05-29 18:54:58 +0300

    [Workflow] Fix some error

--- main/enigma.cpp
+++ main/enigma.cpp
@@ -366,7 +366,7 @@ void dump_malloc_stats(void)
 #ifdef __GLIBC__
 #if __GLIBC__ > 2 || (__GLIBC__ == 2 && __GLIBC_MINOR__ >= 33)
 	struct mallinfo2 mi = mallinfo2();
-	eDebug("[Enigma] MALLOC: %zu total", mi.uordblks);
+	eDebug("[Enigma] MALLOC: %u total", mi.uordblks);
 #else
 	struct mallinfo mi = mallinfo();
 	eDebug("[Enigma] MALLOC: %d total", mi.uordblks);
