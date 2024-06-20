61a15ee7ba47dbe620324bb580bbb75c263731f7
Boris <boris.fox.solovyev@gmail.com>
2024-06-20 20:16:32 +0300

    [hosttools] * fix sonar issues

--- tools/host_tools/FormatConverter/main.py
+++ tools/host_tools/FormatConverter/main.py
@@ -1,9 +1,11 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/python
+from os import system
+
 from datasource import genericdatasource
 from satxml import satxml
 from lamedb import lamedb
 from input import *
-from os import system
+
 
 maindata = genericdatasource()
 
@@ -19,23 +21,23 @@ for source in datasources:
 
 while True:
 	system("/usr/bin/clear")
-	list = []
-	for index in list(range(len(datasources))):
-		list.append(datasources[index].getName() + f" ({len(datasources[index].transponderlist.keys())} sats)")
-	index = inputChoices(list, "q", "quit")
+	items = []
+	for index in range(len(datasources)):
+		items.append(datasources[index].getName() + f" ({len(datasources[index].transponderlist.keys())} sats)")
+	index = inputChoices(items, "q", "quit")
 	if index is None:
 		break
 
 	while True:
 		print(datasources[index].getStatus())
-		list = []
+		items = []
 		for action in datasources[index].getCapabilities():
-			list.append(action[0])
-		action = inputChoices(list)
+			items.append(action[0])
+		action = inputChoices(items)
 		if action is None:
 			break
 
 		datasources[index].getCapabilities()[action][1]()
-		# except:
-		# print(sys.exc_info())
-		# print("sorry, could not execute that command")
+		#except:
+		#	print sys.exc_info()
+		#	print "sorry, could not execute that command"
