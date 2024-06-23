0b287c8f9e4d86dca1e40c50dd21ef496ae0ee9d
Boris <boris.fox.solovyev@gmail.com>
2024-06-23 23:53:19 +0300

    Fix typo.

--- lib/service/servicedvb.cpp
+++ lib/service/servicedvb.cpp
@@ -1491,7 +1491,7 @@ RESULT eDVBServicePlay::setTarget(int target, bool noaudio = false)
 	// start/stop audio
 	if (target == 1000)
 	{
-		if (noaгdio) // stop audio
+		if (noaudio) // stop audio
 		{
 			if (m_decoder && !m_noaudio)
 			{
