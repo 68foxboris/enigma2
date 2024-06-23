07f78451e388ab7d663877531a2990a4ef3050f9
Boris <boris.fox.solovyev@gmail.com>
2024-06-24 00:04:25 +0300

    Fix typo.

--- lib/python/Screens/InfoBarGenerics.py
+++ lib/python/Screens/InfoBarGenerics.py
@@ -391,7 +391,7 @@ class InfoBarShowHide(InfoBarScreenSaver):
 			self.unDimmingTimer = eTimer()
 			self.unDimmingTimer.callback.append(self.unDimming)
 
-		self.hideTimer = eTimer()Co-Authored-By: jbleyel <jbleyel@gmx.net>
+		self.hideTimer = eTimer()
 		self.hideTimer.callback.append(self.doTimerHide)
 		self.hideTimer.start(5000, True)
 
