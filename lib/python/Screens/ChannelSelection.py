27ea03d544d3158f83597ad66920f9e48bce5aa9
Boris <boris.fox.solovyev@gmail.com>
2024-10-14 19:05:14 +0300

    In ChannelSelection when zero does nothing show that
    Maybe later we can also consider to add a panic function to zero here to
    zap to first bouquet, first channel. But I suggest when done then
    perform it via a config.
    
    Co-Authored-By: littlesat <littlesat99@yahoo.com>

--- lib/python/Screens/ChannelSelection.py
+++ lib/python/Screens/ChannelSelection.py
@@ -2030,6 +2030,8 @@ class ChannelSelectionBase(Screen):
 			self.keyNumberGlobal(number)
 		elif len(self.servicePath) > 1:
 			self.keyGoUp()
+		else:
+			return False
 
 	def keyNumberGlobal(self, number):
 		if self.isBasePathEqual(self.bouquet_root):
