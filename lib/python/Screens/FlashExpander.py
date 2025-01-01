53c95eec163f12ff4d64d83fbd06de187a9a669d
Boris <boris.fox.solovyev@gmail.com>
2025-01-01 08:54:09 +0200

    * sanity callback
    Co-Authored-By: jbleyel <jbleyel@gmx.net>

--- lib/python/Screens/FlashExpander.py
+++ lib/python/Screens/FlashExpander.py
@@ -339,7 +339,7 @@ class FlashExpander(Setup):
 						if deviceID:
 							self.deviceData[deviceID] = (data[0][:-1], name)
 			self.updateStatus()
-			if callback:
+			if callback and callable(callback):
 				callback()
 
 		self.console.ePopen(["/sbin/blkid", "/sbin/blkid"], callback=getBlockIdDataCallback)
