8741f4467e93e4ce7d8301a0a98f2858a12aa5c2
Boris <boris.fox.solovyev@gmail.com>
2025-01-05 15:31:45 +0200

    [DeviceManager] Update Summary
    Co-Authored-By: fairbird <rrrr53@hotmail.com>

--- lib/python/Screens/DeviceManager.py
+++ lib/python/Screens/DeviceManager.py
@@ -1057,8 +1057,9 @@ class DevicesPanelSummary(Screen):
 	def __init__(self, session, parent):
 		Screen.__init__(self, session, parent=parent)
 		self.skinName = "SetupSummary"
-		self["entry"] = StaticText("")
-		self["value"] = StaticText("")
+		self["SetupTitle"] = StaticText("")
+		self["SetupEntry"] = StaticText("")
+		self["SetupValue"] = StaticText("")
 		self.onShow.append(self.addWatcher)
 		self.onHide.append(self.removeWatcher)
 
@@ -1070,5 +1071,6 @@ class DevicesPanelSummary(Screen):
 		self.parent.onChangedEntry.remove(self.selectionChanged)
 
 	def selectionChanged(self, name, desc):
-		self["entry"].text = name
-		self["value"].text = desc
+		self["SetupTitle"].text = name
+		self["SetupEntry"].text = name
+		self["SetupValue"].text = desc
