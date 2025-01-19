096643fa398718c030bb363f9d2c6abaa7fe3b85
Boris <boris.fox.solovyev@gmail.com>
2025-01-19 19:40:31 +0200

    [AudioSelection] Fix typo

--- lib/python/Screens/AudioSelection.py
+++ lib/python/Screens/AudioSelection.py
@@ -261,7 +261,7 @@ class AudioSelection(ConfigListScreen, Screen):
 				self.settings.surround_3d_speaker.addNotifier(self.change3DSurroundSpeaker, initial_call=False)
 				conflist.append(getConfigListEntry(_("3D Surround Speaker Position"), self.settings.surround_3d_speaker, None))
 
-			if BoxInfo.getItem("CanAutoVolume"):
+			if BoxInfo.getItem("HasAutoVolume"):
 				choice_list = [("none", _("Off")), ("hdmi", _("HDMI")), ("spdif", _("SPDIF")), ("dac", _("DAC"))]
 				self.settings.autovolume = ConfigSelection(choices=choice_list, default=config.av.autovolume.value)
 				self.settings.autovolume.addNotifier(self.changeAutoVolume, initial_call=False)
