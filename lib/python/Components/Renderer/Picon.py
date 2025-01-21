8be72440161146b59580a6c49a08cac3cf0ebe11
Boris <boris.fox.solovyev@gmail.com>
2025-01-21 06:12:54 +0200

    [Picon] Add Option to disable picons on the Infobar
    Co-Authored-By: fairbird <rrrr53@hotmail.com>

--- lib/python/Components/Renderer/Picon.py
+++ lib/python/Components/Renderer/Picon.py
@@ -1,90 +1,96 @@
-from os.path import isdir, join
+# -*- coding: utf-8 -*-
 from os import listdir
+from os.path import exists, getsize, isdir, join
 from re import sub
-import unicodedata
-from Components.Renderer.Renderer import Renderer
+from unicodedata import normalize
 from enigma import ePixmap
-from Tools.Alternatives import GetWithAlternative
-from Tools.Directories import pathExists, SCOPE_SKIN_IMAGE, SCOPE_CURRENT_SKIN, resolveFilename, sanitizeFilename
+from Components.config import config
 from Components.Harddisk import harddiskmanager
+from Components.Renderer.Renderer import Renderer
 from ServiceReference import ServiceReference
+from Tools.Alternatives import GetWithAlternative
+from Tools.Directories import SCOPE_SKIN_IMAGE, SCOPE_CURRENT_SKIN, resolveFilename, sanitizeFilename
 
 
 class PiconLocator:
-	def __init__(self):
-		harddiskmanager.on_partition_list_change.append(self.__onPartitionChange)
-		self.piconDirectories = "picon"
+	def __init__(self, piconDirectories=["picon"]):
+		harddiskmanager.on_partition_list_change.append(self.onPartitionChange)
+		self.piconDirectories = piconDirectories
 		self.activePiconPath = None
 		self.searchPaths = []
 		for mp in ("/usr/share/enigma2/", "/"):
-			self.__onMountpointAdded(mp)
+			self.onMountpointAdded(mp)
 		for part in harddiskmanager.getMountedPartitions():
-			self.__onMountpointAdded(part.mountpoint)
+			mp = join(part.mountpoint, "usr/share/enigma2")
+			self.onMountpointAdded(part.mountpoint)
+			self.onMountpointAdded(mp)
 
-	def __onMountpointAdded(self, mountpoint):
-		try:
-			path = join(mountpoint, self.piconDirectories) + "/"
-			if isdir(path) and path not in self.searchPaths:
-				for fn in listdir(path):
-					if fn.endswith(".png") or fn.endswith(".svg"):
-						print(f"[Picon] adding path: {path}")
-						self.searchPaths.append(path)
-						break
-		except:
-			pass
 
-	def __onMountpointRemoved(self, mountpoint):
-		path = join(mountpoint, self.piconDirectories) + "/"
-		try:
-			self.searchPaths.remove(path)
-			print(f"[Picon] removed path: {path}")
-		except:
-			pass
+	def onMountpointAdded(self, mountpoint):
+		for piconDirectory in self.piconDirectories:
+			try:
+				path = join(mountpoint, piconDirectory) + "/"
+				if isdir(path) and path not in self.searchPaths:
+					for fn in listdir(path):
+						if fn.endswith(".png") or fn.endswith(".svg"):
+							print(f"[Picon] adding path: {path}")
+							self.searchPaths.append(path)
+							break
+			except Exception as err:
+				print(f"[Picon] Failed to investigate {mountpoint}:{str(err)}")
 
-	def __onPartitionChange(self, why, part):
+	def onMountpointRemoved(self, mountpoint):
+		for piconDirectory in self.piconDirectories:
+			path = join(mountpoint, piconDirectory) + "/"
+			try:
+				self.searchPaths.remove(path)
+				print(f"[Picon] removed path: {path}")
+			except Exception:
+				pass
+
+	def onPartitionChange(self, why, part):
 		if why == "add":
-			self.__onMountpointAdded(part.mountpoint)
+			self.onMountpointAdded(part.mountpoint)
 		elif why == "remove":
-			self.__onMountpointRemoved(part.mountpoint)
+			self.onMountpointRemoved(part.mountpoint)
 
 	def findPicon(self, serviceName):
 		if self.activePiconPath is not None:
 			for ext in (".png", ".svg"):
-				pngname = self.activePiconPath + serviceName + ext
-				if pathExists(pngname):
-					return pngname
+				pngname = f"{self.activePiconPath}{serviceName}{ext}"
+				return pngname if exists(pngname) else ""
 		else:
 			for path in self.searchPaths:
 				for ext in (".png", ".svg"):
-					pngname = path + serviceName + ext
-					if pathExists(pngname):
+					pngname = f"{path}{serviceName}{ext}"
+					if exists(pngname):
 						self.activePiconPath = path
 						return pngname
 		return ""
 
 	def addSearchPath(self, value):
-		if pathExists(value):
+		if exists(value):
 			if not value.endswith("/"):
 				value += "/"
 			if not value.startswith("/media/net") and not value.startswith("/media/autofs") and value not in self.searchPaths:
 				self.searchPaths.append(value)
 
 	def getPiconName(self, serviceName):
-		# remove the path and name fields, and replace ":" by "_"
+		#remove the path and name fields, and replace ":" by "_"
 		fields = GetWithAlternative(serviceName).split(":", 10)[:10]
 		if not fields or len(fields) < 10:
 			return ""
 		pngname = self.findPicon("_".join(fields))
 		if not pngname and not fields[6].endswith("0000"):
-			# remove "sub-network" from namespace
+			#remove "sub-network" from namespace
 			fields[6] = fields[6][:-4] + "0000"
 			pngname = self.findPicon("_".join(fields))
 		if not pngname and fields[0] != "1":
-			# fallback to 1 for IPTV streams
+			#fallback to 1 for IPTV streams
 			fields[0] = "1"
 			pngname = self.findPicon("_".join(fields))
 		if not pngname and fields[2] != "2":
-			# fallback to 1 for TV services with non-standard service types
+			#fallback to 1 for TV services with non-standard service types
 			fields[2] = "1"
 			pngname = self.findPicon("_".join(fields))
 		if not pngname: # picon by channel name
@@ -97,10 +103,10 @@ class PiconLocator:
 		if not pngname: # picon default
 			tmp = resolveFilename(SCOPE_CURRENT_SKIN, "picon_default.png") # picon_default in current active skin
 			tmp2 = self.findPicon("picon_default") # picon_default in picon folder
-			if pathExists(tmp2):
+			if exists(tmp2):
 				pngname = tmp2
 			else:
-				if pathExists(tmp):
+				if exists(tmp):
 					pngname = tmp
 				else:
 					pngname = resolveFilename(SCOPE_CURRENT_SKIN, "picon_default.png")
@@ -113,6 +119,7 @@ piconLocator = None
 def initPiconPaths():
 	global piconLocator
 	piconLocator = PiconLocator()
+
 
 initPiconPaths()
 
@@ -144,7 +151,9 @@ class Picon(Renderer):
 		if self.instance:
 			if what[0] in (self.CHANGED_DEFAULT, self.CHANGED_ALL, self.CHANGED_SPECIFIC):
 				pngname = piconLocator.getPiconName(self.source.text)
-				if not pathExists(pngname):  # no picon for service found
+				if not exists(pngname): # no picon for service found
+					pngname = self.defaultpngname
+				if not config.usage.showpicon.value: # disabe picon on infobar
 					pngname = self.defaultpngname
 				if self.pngname != pngname:
 					if pngname:
