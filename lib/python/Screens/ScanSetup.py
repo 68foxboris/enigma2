4734296a75d53ff928d04cb994ed42d87516841c
Boris <boris.fox.solovyev@gmail.com>
2025-01-21 19:30:19 +0200

    "[SatConfig]" use on_change for ConfigListScreen
    Instead of hacking around with some objects from the ConfigListScreen
    
    Co-Authored-By: littlesat <littlesat99@yahoo.com>

--- lib/python/Screens/ScanSetup.py
+++ lib/python/Screens/ScanSetup.py
@@ -641,7 +641,7 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport, Terrest
 		self.statusTimer.callback.append(self.updateStatus)
 
 		self.list = []
-		ConfigListScreen.__init__(self, self.list, fullUI=True)
+		ConfigListScreen.__init__(self, self.list, on_change=self.newConfig, fullUI=True)
 		self["introduction"] = Label("")
 		if self.scan_nims.value == "":
 			self["introduction"].text = _("Nothing to scan! Setup your tuner and try again.")
@@ -1272,23 +1272,6 @@ class ScanSetup(ConfigListScreen, Screen, CableTransponderSearchSupport, Terrest
 		self.scan_terrestrial_binary_scan = ConfigYesNo(default=False)
 
 		return True
-
-	def keyLeft(self):
-		ConfigListScreen.keyLeft(self)
-		self.newConfig()
-
-	def keyRight(self):
-		ConfigListScreen.keyRight(self)
-		self.newConfig()
-
-	def handleKeyFileCallback(self, answer):
-		ConfigListScreen.handleKeyFileCallback(self, answer)
-		self.newConfig()
-
-	def keySelectionCallback(self, answer):
-		if answer:
-			ConfigListScreen.keySelectionCallback(self, answer)
-			self.newConfig()
 
 	def updateStatus(self):
 		print("[ScanSetup] updatestatus")
