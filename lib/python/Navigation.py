d4165775ea2a271092ca92f05b3d8ab178cff4e3
Boris <boris.fox.solovyev@gmail.com>
2025-03-11 20:54:43 +0200

    [Navigation] more check set setWasInDeepStandby() (#4199)
    Co-Authored-By: Dima73 <Dima-73@inbox.lv>

--- lib/python/Navigation.py
+++ lib/python/Navigation.py
@@ -60,7 +60,7 @@ class Navigation:
 			ImportChannels()
 		if self.__wasTimerWakeup:
 			self.wakeup_timer_enabled = wakeup_time_type == 3 and self.__prevWakeupTime
-			if not self.wakeup_timer_enabled:
+			if self.__prevWakeupTime and wakeup_time_type in (0, 1) and not config.misc.RestartUI.value:
 				RecordTimer.RecordTimerEntry.setWasInDeepStandby()
 		if config.misc.RestartUI.value:
 			config.misc.RestartUI.value = False
@@ -69,7 +69,7 @@ class Navigation:
 		else:
 			if config.usage.remote_fallback_import.value and not config.usage.remote_fallback_import_restart.value:
 				ImportChannels()
-			if startup_to_standby == "yes" or (self.__wasTimerWakeup and self.__prevWakeupTime and (wakeup_time_type == 0 or wakeup_time_type == 1 or (wakeup_time_type == 3 and startup_to_standby == "except"))):
+			if startup_to_standby == "yes" or (self.__wasTimerWakeup and self.__prevWakeupTime and (wakeup_time_type in (0, 1) or (wakeup_time_type == 3 and startup_to_standby == "except"))):
 				if not Screens.Standby.inTryQuitMainloop:
 					self.standbytimer = eTimer()
 					self.standbytimer.callback.append(self.gotostandby)
