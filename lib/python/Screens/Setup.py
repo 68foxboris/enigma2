9433d7cdfd42dd97c0cd727156bd6b6efa7d45a8
Boris <boris.fox.solovyev@gmail.com>
2025-04-25 19:57:39 +0300

    [Setup] * improve indent
    Co-Authored-By: jbleyel <jbleyel@gmx.net>

--- lib/python/Screens/Setup.py
+++ lib/python/Screens/Setup.py
@@ -89,7 +89,7 @@ class Setup(ConfigListScreen, Screen, HelpableScreen):
 					self["config"].list.sort(key=lambda x: x[0])
 				self.moveToItem(currentItem)
 
-	def addItems(self, parentNode, including=True, indent=0):
+	def addItems(self, parentNode, including=True, indent=""):
 		for element in parentNode:
 			if not element.tag:
 				continue
@@ -100,8 +100,7 @@ class Setup(ConfigListScreen, Screen, HelpableScreen):
 				if including and include:
 					self.addItem(element, indent=indent)
 			elif element.tag == "if":
-				indent = element.get("indent", "")
-				indent = int(indent) if indent and indent.isnumeric() and int(indent) > 0 else None
+				indent = element.get("indent", indent)
 				if including:
 					self.addItems(element, including=include, indent=indent)
 			elif element.tag == "elif":
@@ -109,7 +108,7 @@ class Setup(ConfigListScreen, Screen, HelpableScreen):
 			elif element.tag == "else":
 				including = True
 
-	def addItem(self, element, indent=0):
+	def addItem(self, element, indent=""):
 		indent = parameters.get("SetupIndent", "  ") * int(element.get("indents", 0))
 		if self.pluginLanguageDomain:
 			itemText = indent + (dgettext(self.pluginLanguageDomain, x) if (x := element.get("text")) else "* fix me *")
@@ -118,8 +117,8 @@ class Setup(ConfigListScreen, Screen, HelpableScreen):
 			itemText = indent + (_(x) if (x := element.get("text")) else "* fix me *")
 			itemDescription = _(x) if (x := element.get("description")) else ""
 		restart = element.get("restart", "").lower()
-		indent = element.get("indent", "") or str(indent)
-		indent = int(indent) if indent and indent.isnumeric() and int(indent) > 0 else None
+		indent = element.get("indent", indent)
+		indent = int(indent) if indent and indent.isnumeric() else None
 		if restart == "gui" and not itemText.endswith("*"):  # Add "*" as restart indicator based on the restart attribute.
 			itemText = f"{itemText}*"
 		elif restart == "system" and not itemText.endswith("#"):  # Add "#" as reboot indicator based on the restart attribute.
@@ -128,10 +127,8 @@ class Setup(ConfigListScreen, Screen, HelpableScreen):
 		if item == "":
 			self.list.append((self.formatItemText(itemText),))  # Add the comment line to the config list.
 		elif not isinstance(item, ConfigNothing):
-			if indent:
-				self.list.append(((self.formatItemText(itemText), indent), item, self.formatItemDescription(item, itemDescription)))  # Add the item to the config list.
-			else:
-				self.list.append((self.formatItemText(itemText), item, self.formatItemDescription(item, itemDescription)))  # Add the item to the config list.
+			label = (self.formatItemText(itemText), indent) if indent else self.formatItemText(itemText)
+			self.list.append((label, item, self.formatItemDescription(item, itemDescription)))  # Add the item to the config list.
 		if item is config.usage.setupShowDefault:
 			self.showDefaultChanged = True
 		if item is config.usage.boolean_graphic:
