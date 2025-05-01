517eeb63be12aac9edef5b878b89c7b41fc51bc0
Boris <boris.fox.solovyev@gmail.com>
2025-05-01 19:00:51 +0300

    Single EPG - timespan for filtering EPG can be changed online (#4217)
    - change hour interval with 1/4 and 3/6 keys
    - changes can be temporary or permanent (setup)
    - keys for filtering added and moved to standalone key actions
    
    Co-Authored-By: ims <ims21@users.sourceforge.net>

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -1486,6 +1486,7 @@ def InitUsageConfig():
 	config.epg.filter_end.addNotifier(validateEPGFilterTimes)
 	config.epg.filter_reversal = ConfigYesNo(default=False)
 	config.epg.filter_keepsorting = ConfigYesNo(default=False)
+	config.epg.filter_restore_timespan = ConfigYesNo(default=True)
 
 	def correctInvalidEPGDataChange(configElement):
 		eServiceEvent.setUTF8CorrectMode(int(configElement.value))
