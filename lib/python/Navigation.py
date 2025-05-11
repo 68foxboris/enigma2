24652b57df02c7139e427df68e5e56902f1ad61d
Boris <boris.fox.solovyev@gmail.com>
2025-05-11 18:29:54 +0300

    [Navigation]
    * introduce new functions for recording counts
    Co-Authored-By: jbleyel <jbleyel@gmx.net>

--- lib/python/Navigation.py
+++ lib/python/Navigation.py
@@ -12,6 +12,7 @@ from Components.config import config
 from Components.ImportChannels import ImportChannels
 from Components.ParentalControl import parentalControl
 from Components.PluginComponent import plugins
+from Components.RecordingConfig import recType
 from Components.SystemInfo import BoxInfo
 from Plugins.Plugin import PluginDescriptor
 from Screens.InfoBar import InfoBar, MoviePlayer
@@ -69,7 +70,7 @@ class Navigation:
 		self.Scheduler = Scheduler.Scheduler()  # Initialize Scheduler before RecordTimer.loadTimers.
 		if not self.RecordTimer:
 			self.RecordTimer = RecordTimer.RecordTimer()
-			self.RecordTimer.loadTimer()  # Call loadTimer after initialize of self.RecordTimer.
+			self.RecordTimer.loadTimers()  # Call loadTimers after initialize of self.RecordTimer.
 			self.isRecordTimerImageStandard = True
 		self.Scheduler.loadTimers()  # Call loadTimers after initialize of self.Scheduler.
 		self.__wasTimerWakeup = False
@@ -541,6 +542,15 @@ class Navigation:
 		if not self.currentlyPlayingService:
 			self.currentlyPlayingService = self.pnav and self.pnav.getCurrentService()
 		return self.currentlyPlayingService
+
+	def getAnyRecordingsCount(self):
+		return len(self.getRecordings(False, pNavigation.isAnyRecording))
+
+	def getIndicatorRecordingsCount(self):
+		return len(self.getRecordings(False, recType(config.recording.show_rec_symbol_for_rec_types.getValue())))
+
+	def getRealRecordingsCount(self):
+		return len(self.getRecordings(False, pNavigation.isRealRecording))
 
 	def stopService(self):
 		if self.pnav:
