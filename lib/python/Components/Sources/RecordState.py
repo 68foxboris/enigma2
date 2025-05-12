aef84d69e70c5532176ae569e2c66c7e0b27872a
Boris <boris.fox.solovyev@gmail.com>
2025-05-12 16:19:44 +0300

    Fix NameError: name 'Components' is not defined
    14:08:49.9256   File "/usr/lib/enigma2/python/Screens/SessionGlobals.py", line 32, in __init__
    14:08:49.9257   File "/usr/lib/enigma2/python/Components/Sources/RecordState.py", line 14, in __init__
    14:08:49.9257   File "/usr/lib/enigma2/python/Components/Sources/RecordState.py", line 19, in gotRecordEvent
    14:08:49.9259 NameError: name 'Components' is not defined
    14:08:49.9262 [StartEnigma] Exiting via quitMainloop #4.

--- lib/python/Components/Sources/RecordState.py
+++ lib/python/Components/Sources/RecordState.py
@@ -1,25 +1,27 @@
 # -*- coding: utf-8 -*-
-from Components.Sources.Source import Source
-from Components.Element import cached
 from enigma import iRecordableService
+from Components.Element import cached
+import Components.RecordingConfig
+from Components.Sources.Source import Source
+from Components.config import config
 from Components.SystemInfo import BoxInfo
 
 
 class RecordState(Source):
 	def __init__(self, session):
 		Source.__init__(self)
-		self.records_running = 0
 		self.session = session
+		self.records_running = 0
 		session.nav.record_event.append(self.gotRecordEvent)
-		self.gotRecordEvent(None, None)  # get initial state
+		self.gotRecordEvent(None, None)  # Get initial state.
 
 	def gotRecordEvent(self, service, event):
 		prev_records = self.records_running
 		if event in (iRecordableService.evEnd, iRecordableService.evStart, None):
-			recs = self.session.nav.getRecordings(False, Components.RecordingConfig.recType(config.recording.show_rec_symbol_for_rec_types.getValue()))
+			recordings = self.session.nav.getRecordings(False, Components.RecordingConfig.recType(config.recording.show_rec_symbol_for_rec_types.getValue()))
 			if BoxInfo.getItem("LCDsymbol_circle_recording"):
 				open(BoxInfo.getItem("LCDsymbol_circle_recording"), "w").write(recs and "1" or "0")
-			self.records_running = len(recs)
+			self.records_running = len(recordings)
 			if self.records_running != prev_records:
 				self.changed((self.CHANGED_ALL,))
 
