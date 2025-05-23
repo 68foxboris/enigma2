111040e156224991722f694463676f362098fdb0
Boris <boris.fox.solovyev@gmail.com>
2025-05-23 22:29:14 +0300

    [FrontPanelLed]
    * use new recoding count function
    
    Co-Authored-By: jbleyel <jbleyel@gmx.net>

--- lib/python/Components/FrontPanelLed.py
+++ lib/python/Components/FrontPanelLed.py
@@ -21,10 +21,10 @@ colors = [
 	("0xFFFFFF", _("white")),
 ]
 # running
-config.fp.led.default_color = ConfigSelection(colors, default="0x00FF00")
+config.fp.led.default_color = ConfigSelection(colors, default="0xFFFFFF")
 config.fp.led.default_brightness = ConfigSlider(default=0xff, increment=25, limits=(0, 0xff))
 # standby
-config.fp.led.standby_color = ConfigSelection(colors, default="0xFF0000")
+config.fp.led.standby_color = ConfigSelection(colors, default="0xFFFFFF")
 config.fp.led.standby_brightness = ConfigSlider(default=0x08, increment=8, limits=(0, 0xff))
 # shutdown
 config.fp.led.shutdown_color = ConfigSelection(colors, default="0xFF5500")
@@ -78,11 +78,10 @@ class FrontPanelLed:
 		if not self._session:
 			return
 		if event == iRecordableService.evEnd:
-			recordings = self._session.nav.getRecordings()
-			if not recordings:
-				FrontPanelLed.stopRecording()
-			else:
+			if self._session.nav.getAnyRecordingsCount():
 				FrontPanelLed.recording()
+			else:
+				FrontPanelLed.stopRecording()
 		elif event == iRecordableService.evStart:
 			FrontPanelLed.recording()
 
