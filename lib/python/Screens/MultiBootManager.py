5b62109553d8bdb48f22257e715251e90f4f1683
Boris <boris.fox.solovyev@gmail.com>
2025-08-01 21:26:11 +0300

    [MultiBootManager] update calc for max slots
    Co-Authored-By: atvcaptain <openatv@gmail.com>

--- lib/python/Screens/MultiBootManager.py
+++ lib/python/Screens/MultiBootManager.py
@@ -1,3 +1,4 @@
+from math import ceil
 from os import W_OK, access, listdir, remove, stat, statvfs
 from os.path import exists, isdir, join, realpath
 from re import compile
@@ -816,6 +817,7 @@ class ChkrootInit(Screen):
 
 		self.session.openWithCallback(disableChkrootCallback, MessageBox, _("Permanently disable the MultiBoot option?"), simple=True)
 
+
 class ChkrootSlotManager(Setup):
 	def __init__(self, session):
 		def getGreenHelpText():
@@ -968,7 +970,7 @@ class ChkrootSlotManager(Setup):
 			path = path if exists(path) else f"/sys/block/{base}/size"
 			with open(path) as fd:
 				blocks = int(fd.read().strip())
-				return (blocks * 512) // (1024 * 1024 * 1024)
+				return ceil((blocks * 512) / (1024 * 1024 * 1024))
 		except Exception as e:
 			return 0
 
@@ -1144,7 +1146,7 @@ class UBISlotManager(Setup):
 			path = path if exists(path) else f"/sys/block/{base}/size"
 			with open(path) as fd:
 				blocks = int(fd.read().strip())
-				return (blocks * 512) // (1024 * 1024 * 1024)
+				return ceil((blocks * 512) / (1024 * 1024 * 1024))
 		except Exception as e:
 			return 0
 
