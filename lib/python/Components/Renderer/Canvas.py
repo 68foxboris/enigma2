ec818fd58949e1ecf960f49070822476173f56cf
Boris <boris.fox.solovyev@gmail.com>
2025-08-07 17:19:59 +0300

    Canvas - sanity fix – cast draw parameters to int (Python 3 stricter than Py2)
    Co-Authored-By: ims <ims21@users.sourceforge.net>

--- lib/python/Components/Renderer/Canvas.py
+++ lib/python/Components/Renderer/Canvas.py
@@ -1,5 +1,6 @@
-from enigma import eCanvas, eRect, eSize, gRGB
 from Components.Renderer.Renderer import Renderer
+
+from enigma import eCanvas, eRect, gRGB
 
 
 class Canvas(Renderer):
@@ -8,44 +9,53 @@ class Canvas(Renderer):
 	def __init__(self):
 		Renderer.__init__(self)
 		self.sequence = None
-		self.drawCount = 0
+		self.draw_count = 0
 
-	def pullUpdates(self):
-		if self.instance is not None:  # do an incremental update
-			items = self.source.drawlist
-			if items is not None:
-				if items[0] != self.sequence:  # if the lists sequence count changed, re-start from begin
-					self.sequence = items[0]
-					self.drawCount = 0
-				self.draw(items[1][self.drawCount:])
-				self.drawCount = len(items[1])
+	def pull_updates(self):
+		if self.instance is None:
+			return
 
-	def draw(self, items):
-		for item in items:
-			if item[0] == 1:
-				self.instance.fillRect(eRect(item[1], item[2], item[3], item[4]), gRGB(item[5]))
-			elif item[0] == 2:
-				self.instance.writeText(eRect(item[1], item[2], item[3], item[4]), gRGB(item[5]), gRGB(item[6]), item[7], item[8], item[9])
-			elif item[0] == 3:
-				self.instance.drawLine(item[1], item[2], item[3], item[4], gRGB(item[5]))
-			elif item[0] == 4:
-				self.instance.drawRotatedLine(item[1], item[2], item[3], item[4], item[5], item[6], item[7], item[8], gRGB(item[9]))
+		# do an incremental update
+		list = self.source.drawlist
+		if list is None:
+			return
+
+		# if the lists sequence count changed, re-start from begin
+		if list[0] != self.sequence:
+			self.sequence = list[0]
+			self.draw_count = 0
+
+		self.draw(list[1][self.draw_count:])
+		self.draw_count = len(list[1])
+
+	def draw(self, list):
+		for l in list:
+			if l[0] == 1:
+				self.instance.fillRect(eRect(l[1], l[2], l[3], l[4]), gRGB(l[5]))
+			elif l[0] == 2:
+				self.instance.writeText(eRect(l[1], l[2], l[3], l[4]), gRGB(l[5]), gRGB(l[6]), l[7], l[8], l[9])
+			elif l[0] == 3:
+				self.instance.drawLine(int(l[1]), int(l[2]), int(l[3]), int(l[4]), gRGB(l[5]))
+			elif l[0] == 4:
+				self.instance.drawRotatedLine(int(l[1]), int(l[2]), int(l[3]), int(l[4]), int(l[5]), int(l[6]), l[7], l[8], gRGB(l[9]))
 			else:
-				print(f"[Canvas] drawlist entry: {str(item)}")
+				print("drawlist entry:", l)
 				raise RuntimeError("invalid drawlist entry")
 
 	def changed(self, what):
-		self.pullUpdates()
+		self.pull_updates()
 
 	def postWidgetCreate(self, instance):
 		self.sequence = None
 
-		def parseSize(val):
-			x, y = val.split(',')
+		from enigma import eSize
+
+		def parseSize(str):
+			x, y = str.split(',')
 			return eSize(int(x), int(y))
 
 		for (attrib, value) in self.skinAttributes:
 			if attrib == "size":
 				self.instance.setSize(parseSize(value))
 
-		self.pullUpdates()
+		self.pull_updates()
