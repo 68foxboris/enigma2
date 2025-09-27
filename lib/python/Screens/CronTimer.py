fbda384e2b4adf9a4b2d1c58415343f2f0b27997
Boris <boris.fox.solovyev@gmail.com>
2025-09-27 22:38:03 +0300

    text updates
    Co-Authored-By: jbleyel <jbleyel@gmx.net>

--- lib/python/Screens/CronTimer.py
+++ lib/python/Screens/CronTimer.py
@@ -119,7 +119,7 @@ class CronTimers(Screen):
 				name = ""
 		except:
 			name = ""
-		desc = _("Current status:") + " " + self.summary_running
+		desc = _("Current Status:") + " " + self.summary_running
 		for cb in self.onChangedEntry:
 			cb(name, desc)
 
