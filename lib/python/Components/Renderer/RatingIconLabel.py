87ec31d4cc1a592771238c37ac236d212bc286d3
Boris <boris.fox.solovyev@gmail.com>
2025-10-02 17:16:05 +0300

    [Added] Failsafe on getting rating.... (this is workaround to unknown problem with getting mpaa ratings) (#4264)
    Co-Authored-By: Dimitar Tsenev <dimitar_tsenev@outlook.com>

--- lib/python/Components/Renderer/RatingIconLabel.py
+++ lib/python/Components/Renderer/RatingIconLabel.py
@@ -1,34 +1,31 @@
-from enigma import eLabel, gRGB
-
-from skin import parseColor
 from Components.Renderer.Renderer import Renderer
+from enigma import eLabel, gRGB
+from skin import parseColor
 
 
 class RatingIconLabel(Renderer):
-	GUI_WIDGET = eLabel
-
 	def __init__(self):
 		Renderer.__init__(self)
 		self.colors = {}
 		self.extendDirection = "right"
 		self.sidesMargin = 20
 
+	GUI_WIDGET = eLabel
+
 	def postWidgetCreate(self, instance):
-		instance.setNoWrap(True)
 		self.changed((self.CHANGED_DEFAULT,))
 
 	def applySkin(self, desktop, parent):
 		attribs = []
 		for (attrib, value) in self.skinAttributes:
-			match attrib:
-				case "colors":
-					self.colors = {int(k): parseColor(v) for k, v in (item.split(":") for item in value.split(","))}
-				case "extendDirection":
-					self.extendDirection = value
-				case "sidesMargin":
-					self.sidesMargin = int(value)
-				case _:
-					attribs.append((attrib, value))
+			if attrib == "colors":
+				self.colors = {int(k): parseColor(v) for k, v in (item.split(":") for item in value.split(","))}
+			elif attrib == "extendDirection":
+				self.extendDirection = value
+			elif attrib == "sidesMargin":
+				self.sidesMargin = int(value)
+			else:
+				attribs.append((attrib, value))
 		self.skinAttributes = attribs
 		result = Renderer.applySkin(self, desktop, parent)
 		self.changed((self.CHANGED_DEFAULT,))
@@ -49,31 +46,39 @@ class RatingIconLabel(Renderer):
 					color = 0x00000000
 					ageText = ""
 					if ";" in self.source.text:
-						splitText = self.source.text.split(";")
-						if splitText and len(splitText) == 2 and splitText[0]:
-							ageText = splitText[0]
-							color = parseColor(splitText[1])
-						else:
+						split_text = self.source.text.split(";")
+						if not split_text or len(split_text) == 1 or not split_text[0]:
 							self.hideLabel()
 							return
+						ageText = split_text[0]
+						color = int(split_text[1], 16)
 					else:
-						age = int(self.source.text.replace("+", ""))
-						if age <= 15:
-							age += 3
-						ageText = str(age)
-						color = self.colors.get(age, 0x10000000)
+						try:
+							age = int(self.source.text.replace("+", ""))
+							if age <= 15:
+								age += 3
+							ageText = str(age)
+							color = self.colors.get(age, 0x10000000)
+						except:
+							pass
+
 					size = self.instance.size()
 					pos = self.instance.position()
+					self.instance.setNoWrap(1)
 					self.instance.setText(ageText)
 					textSize = self.instance.calculateSize()
+					self.instance.setNoWrap(0)
 					newWidth = textSize.width() + self.sidesMargin
 					if newWidth < size.width():
 						newWidth = size.width()
+
 					if self.extendDirection == "left":
 						rightEdgePos = pos.x() + size.width()
 						self.move(rightEdgePos - newWidth, pos.y())
+
 					if self.extendDirection != "none":
 						self.resize(newWidth, size.height())
+
 					self.instance.setBackgroundColor(gRGB(color))
 					self.instance.show()
 				else:
