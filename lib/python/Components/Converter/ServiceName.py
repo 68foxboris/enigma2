1345816eb6d452b19a6bbb7fb5088f16854e5f2d
Boris <boris.fox.solovyev@gmail.com>
2026-04-15 05:56:44 +0300

    Update ServiceName.py
    Update. Based on OpenPli

--- lib/python/Components/Converter/ServiceName.py
+++ lib/python/Components/Converter/ServiceName.py
@@ -40,12 +40,16 @@ class ServiceName(Converter):
 	@cached
 	def getText(self):
 		service = self.source.service
-		if isinstance(service, iPlayableServicePtr):
+		info = None
+		ref = service
+		if isinstance(service, eServiceReference):
+			info = self.source.info
+		elif isinstance(service, iPlayableServicePtr):
 			info = service and service.info()
 			ref = None
-		else:  # reference
+		else: # reference
 			info = service and self.source.info
-			ref = service
+
 		if not info:
 			return ""
 		if self.type == self.NAME:
@@ -54,31 +58,34 @@ class ServiceName(Converter):
 			return self.getProvider(ref, info)
 		elif self.type == self.REFERENCE or self.type == self.EDITREFERENCE and hasattr(self.source, "editmode") and self.source.editmode:
 			if not ref:
-				return info.getInfoString(iServiceInformation.sServiceref)
+				if self.source.info:
+					sref = hasattr(self.source, "serviceref") and self.source.serviceref
+					nref = resolveAlternate(sref)
+					if nref:
+						sref = nref
+					return sref and sref.toString()
+				refstr = info.getInfoString(iServiceInformation.sServiceref)
+				return refstr
 			nref = resolveAlternate(ref)
 			if nref:
 				ref = nref
 			return ref.toString()
 		elif self.type == self.NUMBER:
-			numservice = self.source.serviceref
-			return self.getNumber(numservice, info)
+			return self.getNumber()
 		elif self.type == self.STREAM_URL:
 			srpart = "//%s:%s/" % (config.misc.softcam_streamrelay_url.getHTML(), config.misc.softcam_streamrelay_port.value)
+			path = ""
 			if not ref:
 				refstr = info.getInfoString(iServiceInformation.sServiceref)
-				path = refstr and eServiceReference(refstr).getPath()
-				if not path.startswith("//") and path.find(srpart) == -1:
-					return path
-				else:
-					return ""
-			path = ref.getPath()
-			return "" if path.startswith("//") and path.find(srpart) == -1 else path
+				path = refstr and refstr.split(":")[10].replace("%3a", ":")
+			else:
+				path = ref.getPath()
+			return "" if path.startswith("//") and path.find(srpart) > -1 and "://" not in path else path
 		elif self.type == self.FORMAT_STRING:
 			name = self.getName(ref, info)
-			numservice = hasattr(self.source, "serviceref") and self.source.serviceref
-			num = numservice and self.getNumber(numservice, info) or ""
+			num = self.getNumber() or ""
 			orbpos, tp_data = self.getOrbitalPos(ref, info)
-			provider = self.getProvider(ref, info, tp_data)
+			provider = self.getProvider(ref, info)
 			tuner_system = ref and info and self.getServiceSystem(ref, info, tp_data)
 			res_str = ""
 			for x in self.parts[1:]:
@@ -94,6 +101,8 @@ class ServiceName(Converter):
 					res_str = self.appendToStringWithSeparator(res_str, tuner_system)
 			return res_str
 
+
+
 	text = property(getText)
 
 	def changed(self, what):
@@ -101,56 +110,65 @@ class ServiceName(Converter):
 			Converter.changed(self, what)
 
 	def getName(self, ref, info):
-		name = ref and info.getName(ref)
-		if name is None:
-			name = info.getName()
+		sref = hasattr(self.source, "serviceref") and self.source.serviceref
+		name = (ref and info.getName(ref)) or (sref and (self.source.info and self.source.info.getName(sref)) or sref.getName())
+		if not name:
+			if not ref:
+				name = info.getName()
+
 		return name.replace('\xc2\x86', '').replace('\xc2\x87', '').replace('_', ' ')
+	
+	def getNumber(self):
+		numservice = hasattr(self.source, "serviceref") and self.source.serviceref
+		cnannelNumInt = numservice and numservice.getChannelNum() or 0
+		channelnum = str(cnannelNumInt) if cnannelNumInt else ""
+		return channelnum
 
-	def getNumber(self, ref, info):
-		if not ref:
-			ref = eServiceReference(info.getInfoString(iServiceInformation.sServiceref))
-		num = ref and ref.getChannelNum() or None
-		if num is None:
-			num = '---'
-		else:
-			num = str(num)
-		return num
-
-	def getProvider(self, ref, info, tp_data=None):
-		if ref:
-			return info.getInfoString(ref, iServiceInformation.sProvider)
-		return info.getInfoString(iServiceInformation.sProvider)
+	def getProvider(self, ref, info):
+		sref = hasattr(self.source, "serviceref") and self.source.serviceref
+		prov = ((ref and hasattr(ref, "getProvider") and ref.getProvider()) or (ref and info.getInfoString(ref, iServiceInformation.sProvider))) or (sref and ref and (self.source.info and self.source.info.getInfoString(sref, iServiceInformation.sProvider)) or (hasattr(sref, "getProvider") and sref.getProvider()))
+		if not prov:
+			if not ref:
+				prov = info.getInfoString(iServiceInformation.sProvider)
+			else:
+				prov = ""
+		return prov
 
 	def getOrbitalPos(self, ref, info):
 		orbitalpos = ""
+		tp_data = None
+		sref = hasattr(self.source, "serviceref") and self.source.serviceref
 		if ref:
 			tp_data = info.getInfoObject(ref, iServiceInformation.sTransponderData)
-		else:
+		elif not self.source.info:
 			tp_data = info.getInfoObject(iServiceInformation.sTransponderData)
+		else:
+			tp_data = sref and self.source.info.getInfoObject(sref, iServiceInformation.sTransponderData)
+			
 
 		if tp_data is not None:
 			try:
 				position = tp_data["orbital_position"]
-				if position > 1800:  # west
-					orbitalpos = "%.1f " % (float(3600 - position) / 10) + _("°W")
+				if position > 1800: # west
+					orbitalpos = "%.1f " %(float(3600 - position)/10) + _("°W")
 				else:
-					orbitalpos = "%.1f " % (float(position) / 10) + _("°E")
+					orbitalpos = "%.1f " %(float(position)/10) + _("°E")
 			except:
 				pass
 		return orbitalpos, tp_data
-
+	
 	def getServiceSystem(self, ref, info, feraw):
 		if ref:
 			sref = info.getInfoObject(ref, iServiceInformation.sServiceref)
 		else:
 			sref = info.getInfoObject(iServiceInformation.sServiceref)
-
+		
 		if not sref:
 			sref = ref.toString()
-
+			
 		if sref and "%3a//" in sref:
 			return "IPTV"
-
+			
 		fedata = ConvertToHumanReadable(feraw)
 
 		return fedata.get("system") or ""
