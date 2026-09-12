15e313fd605d4542a29b7fce4ffeca3bce5615e1
Boris <boris.fox.solovyev@gmail.com>
2026-09-12 20:54:20 +0300

    Improve and reorder the option descriptions for “Startup to Standby”, and adjust its help text and the related help text.
    Co-Authored-By: ims <ims21@users.sourceforge.net>

--- lib/python/Components/UsageConfig.py
+++ lib/python/Components/UsageConfig.py
@@ -623,9 +623,9 @@ def InitUsageConfig():
 
 	config.usage.startup_to_standby = ConfigSelection(default="no", choices=[
 		("no", _("No")),
-		("yes", _("Yes")),
-		("except", _("No, except Wakeup timer")),
-		("restart", _("After any restart")),
+		("except", _("Yes, only wake-up timers")),
+		("yes", _("Yes, after cold start only")),
+		("restart", _("Yes, always"))
 	])
 
 	config.usage.wakeup_enabled = ConfigSelection(default="no", choices=[
